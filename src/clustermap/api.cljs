5e2ec1673db280306a5828bf4e3e04deb4ba8c59
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-01-06 19:23:41 +0000

    api client

--- src/clustermap/api.cljs
+++ src/clustermap/api.cljs
@@ -13,13 +13,32 @@
                 (close! ch)))
     ch))
 
-(defn portfolio-company-sites
+(defn all-portfolio-company-sites
   []
   (go
-   (<! (GET "/api/bvca/portfolio-company-sites"))))
+   (<! (GET "/api/bvca/all-portfolio-company-sites"))))
 
-(defn log-portfolio-company-sites
+(defn all-constituencies
   []
   (go
-   (let [ch (portfolio-company-sites)]
-     (.log js/console (<! ch)))))
+   (<! (GET "/api/bvca/all-constituencies"))))
+
+(defn constituency-detail
+  [id]
+  (go
+   (<! (GET (str "/api/bvca/constituency-detail/" id)))))
+
+(defn all-portfolio-companies-summary
+  []
+  (go
+   (<! (GET "/api/bvca/all-portfolio-companies-summary"))))
+
+(defn portfolio-company-detail
+  [id]
+  (go
+   (<! (GET (str "/api/bvca/portfolio-company-detail/" id)))))
+
+(defn investor-company-detail
+  [id]
+  (go
+   (<! (GET (str "/api/bvca/investor-company-detail/" id)))))

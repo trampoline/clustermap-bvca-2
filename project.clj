0ddc03bcf3e88c519b4fa20c5a80840cfaa2ce6a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-01-07 15:05:12 +0000

    update om

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [domina "1.0.2"]
                  [jayq "2.5.0"]
-                 [om "0.1.0"]
+                 [om "0.1.5"]
                  [sablono "0.1.5"]
                  ]
 

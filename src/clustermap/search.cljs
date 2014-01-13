2685c4e27e61b9adb9200243c292fba1eb5656e0
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-01-13 15:26:54 +0000

    esc-key handling in search component

--- src/clustermap/search.cljs
+++ src/clustermap/search.cljs
@@ -1,28 +1,43 @@
 (ns clustermap.search
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put!]]
-            [sablono.core :as html :refer [html] :include-macros true]))
+            [jayq.core :as jayq :refer [$]]
+            [cljs.core.async :refer [put!]]))
 
+(def ESCAPE_KEY 27)
+(def ENTER_KEY 13)
 
 (defn search-result-link
-  [search-result owner opts]
+  [search-result owner {:keys [comm] :as opts}]
   (om/component
    (dom/li #js {}
-           (dom/a #js {}
+           (dom/a #js {:onClick (fn [e] (put! comm [:select search-result]))}
                   (search-result "name")))))
+
+(defn key-down [e search-results owner comm]
+  (condp == (.-keyCode e)
+    ESCAPE_KEY (some-> (om/get-node owner "search-component") $ .toggle)
+    nil))
 
 (defn search-box
   [comm {:keys [search-results] :as data} owner]
   (let [{:strs [constituencies portfolio_companies investor_companies]} search-results]
     (om/component
-     (dom/div #js {:id "search"}
+     (dom/div #js {:ref "search-component"
+                   :id "search"
+                   :onKeyDown (om/bind key-down search-results owner comm)
+                   }
               (dom/h2 nil "Search")
               (dom/div nil
-                       (dom/input #js {:type "text"
+                       (dom/input #js {:ref "search-field"
+                                       :type "text"
                                        :placeholder "Enter your search"
                                        :onChange (fn [e]  (put! comm [:search (.. e -target -value)]))})
-                       (dom/button #js {:type "reset"} "\u00D7"))
+                       (dom/button #js {:type "reset"
+                                        :onClick (fn [e]
+                                                   (put! comm [:search ""])
+                                                   (set! (.-value (om/get-node owner "search-field")) ""))}
+                                   "\u00D7"))
               (when (or (not-empty constituencies) (not-empty portfolio_companies) (not-empty investor_companies))
                 (dom/div #js {:className "search-results"}
                          (apply dom/ul #js {}

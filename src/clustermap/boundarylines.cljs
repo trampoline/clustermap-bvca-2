b3ed8043392f4cb88017861f3607658fd79236f3
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-02-04 16:19:22 +0000

    fetch and cache boundaryline versions asynchronously

--- src/clustermap/boundarylines.cljs
+++ src/clustermap/boundarylines.cljs
@@ -39,27 +39,37 @@
 (defn fetch-boundaryline
   "fetch a boundaryline for a given tolerance and add it to the cache"
   [app-state korks boundaryline-id tolerance]
-  (let [comm (api/boundarylines boundaryline-id tolerance)
+  (let [comm (api/boundarylines boundaryline-id tolerance :raw true)
         use-korks (if (sequential? korks) korks [korks])
         path (concat use-korks [boundaryline-id tolerance])]
     (go
      (let [bl (<! comm)]
+       (.log js/console (clj->js ["rx" app-state boundaryline-id tolerance bl]))
        (swap! app-state update-in path (fn [old] bl))))))
 
 
 (defn get-or-fetch-best-boundaryline
   "gets the best boundaryline version for a zoom level...
-   if the best isn't available, fetches it and immediately
-   returns the next best available"
-  [app-state korks boundaryline-id zoom]
-  (let [use-korks (if (sequential? korks) korks [korks])
-        boundarylines (get-in @app-state use-korks)
-        bl-versions (get boundarylines boundaryline-id)
-        i-tol (ideal-tolerance zoom)
-        use-tol (best-available-tolerance zoom (keys bl-versions))
-        use-bl (get-in boundarylines [boundaryline-id use-tol])]
+   if the best isn't available, fetches it async and immediately
+   returns the next best available
+   - default : default value to return if nothing currently available
+   - min-zoom : if (<= zoom min-zoom) do nothing and return the default
+   returns [tolerance, boundaryline] or default if nothing is available or
+           (<= zoom min-zoom)"
+  [app-state korks boundaryline-id zoom & {:keys [min-zoom]}]
+  (.log js/console (clj->js ["get-or-fetch" app-state korks boundaryline-id zoom]))
+  (when-not (and min-zoom
+                 (<= zoom min-zoom))
 
-    (when (not= i-tol use-tol)
-      (fetch-boundaryline app-state korks boundaryline-id i-tol))
+    (let [use-korks (if (sequential? korks) korks [korks])
+          boundarylines (get-in @app-state use-korks)
+          bl-versions (get boundarylines boundaryline-id)
+          i-tol (ideal-tolerance zoom)
+          use-tol (best-available-tolerance zoom (keys bl-versions))
+          use-bl (get-in boundarylines [boundaryline-id use-tol])]
 
-    use-bl))
+      (when (not= i-tol use-tol)
+        (fetch-boundaryline app-state korks boundaryline-id i-tol))
+
+      (when use-bl
+        [use-tol use-bl]))))

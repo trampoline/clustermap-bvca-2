6fabf50828f4de35ecde04b88ce9c9263c2be1dc
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-02-14 10:14:59 +0000

    add deltas to overview

--- src/clustermap/components/full_report/overview.cljs
+++ src/clustermap/components/full_report/overview.cljs
@@ -46,19 +46,39 @@
                         :investor-companies (fnum (some-> co-stats :investor_company_count) :default "-")
                         :constituencies (fnum (some-> co-stats :constituency_count) :default "-")
                         :turnover (fmoney (some-> site-stats :turnover :total) :sf 2 :default "-")
-                        :employee-count (fnum (some-> site-stats :employee_count :total) :sf 2 :default "-")})
+                        :turnover-delta-val (some-> site-stats :turnover_delta :total)
+                        :turnover-delta (fmoney (some-> site-stats :turnover_delta :total) :sf 2 :default "-")
+                        :employee-count (fnum (some-> site-stats :employee_count :total) :dec 0 :default "-")
+                        :employee-count-delta-val (some-> site-stats :employee_count_delta :total)
+                        :employee-count-delta (fnum (some-> site-stats :employee_count_delta :total) :dec 0 :default "-")})
      :averages (merge (:averages sel-descrs)
                       {:portfolio-companies "\u00A0"
                        :investor-companies "\u00A0"
                        :constituencies "\u00A0"
                        :turnover (fmoney (some-> site-stats :turnover :mean) :sf 2 :default "-")
-                       :employee-count (fnum (some-> site-stats :employee_count :mean) :dec 0 :default "-")})
+                       :turnover-delta-val (some-> site-stats :turnover_delta :mean)
+                       :turnover-delta (fmoney (some-> site-stats :turnover_delta :mean) :sf 2 :default "-")
+                       :employee-count (fnum (some-> site-stats :employee_count :mean) :dec 0 :default "-")
+                       :employee-count-delta-val (some-> site-stats :employee_count_delta :mean)
+                       :employee-count-delta (fnum (some-> site-stats :employee_count_delta :mean) :dec 0 :default "-")})
      :benchmark (merge (:benchmark sel-descrs)
                        {:portfolio-companies (fnum (some-> all-portfolio-company-stats :portfolio_company_count) :default "-")
                         :investor-companies (fnum (some-> all-portfolio-company-stats :investor_company_count) :default "-")
                         :constituencies (fnum (some-> all-portfolio-company-stats :constituency_count) :default "-")
                         :turnover (fmoney (some-> all-portfolio-company-stats :turnover :mean) :sf 2 :default "-")
-                        :employee-count (fnum (some-> all-portfolio-company-stats :employee_count :mean) :dec 0 :default "-")})}))
+                        :turnover-delta-val (some-> all-portfolio-company-stats :turnover_delta :total)
+                        :turnover-delta (fmoney (some-> all-portfolio-company-stats :turnover_delta :mean) :sf 2 :default "-")
+                        :employee-count (fnum (some-> all-portfolio-company-stats :employee_count :mean) :dec 0 :default "-")
+                        :employee-count-delta-val (some-> all-portfolio-company-stats :employee_count_delta :mean)
+                        :employee-count-delta (fnum (some-> all-portfolio-company-stats :employee_count_delta :mean) :dec 0 :default "-")})}))
+
+(defn pos-neg
+  [value]
+  (cond
+   (nil? value) nil
+   (> value 0) (html [:i.icon-positive])
+   (< value 0) (html [:i.icon-negative])
+   true nil))
 
 (defn overview
   [data]
@@ -76,8 +96,10 @@
                 [:th "Portfolio Companies"]
                 [:th "Investors"]
                 [:th "Constituencies"]
-                [:th "Revenue (\u00A3)"]
-                [:th "Employees"]]]
+                [:th "Revenue"]
+                [:th {:colSpan "2"} "Rev. change"]
+                [:th "Employees"]
+                [:th {:colSpan "2"} "Emp. change"]]]
               [:tbody
                [:tr
                 [:th [:i.icon-info {:data-toggle "tooltip" :data-container "body" :title (selection :description)}] (selection :name)]
@@ -85,19 +107,33 @@
                 [:td [:span.selection (:investor-companies selection)]]
                 [:td [:span.selection (:constituencies selection)]]
                 [:td [:span.selection (:turnover selection)]]
-                [:td [:span.selection (:employee-count selection)]]]
+                [:td [:span.selection (pos-neg (:turnover-delta-val selection))]]
+                [:td [:span.selection (:turnover-delta selection)]]
+                [:td [:span.selection (:employee-count selection)]]
+                [:td [:span.selection (pos-neg (:employee-count-delta-val selection))]]
+                [:td [:span.selection (:employee-count-delta selection)]]]
                [:tr
                 [:th [:i.icon-info {:data-toggle "tooltip" :data-container "body" :title (averages :description)}] (averages :name)]
                 [:td [:span.averages (:portfolio-companies averages)]]
                 [:td [:span.averages (:investor-companies averages)]]
                 [:td [:span.averages (:constituencies averages)]]
                 [:td [:span.averages (:turnover averages)]]
-                [:td [:span.averages (:employee-count averages)]]]
+                [:td [:span.averages (pos-neg (:turnover-delta-val averages))]]
+                [:td [:span.averages (:turnover-delta averages)]]
+                [:td [:span.averages (:employee-count averages)]]
+                [:td [:span.averages (pos-neg (:employee-count-delta-val averages))]]
+                [:td [:span.averages (:employee-count-delta averages)]]
+]
                [:tr
                 [:th [:i.icon-info {:data-toggle "tooltip" :data-container "body" :title (benchmark :description)}] (benchmark :name)]
                 [:td [:span.benchmark (:portfolio-companies benchmark)]]
                 [:td [:span.benchmark (:investor-companies benchmark)]]
                 [:td [:span.benchmark (:constituencies benchmark)]]
                 [:td [:span.benchmark (:turnover benchmark)]]
-                [:td [:span.benchmark (:employee-count benchmark)]]]
+                [:td [:span.benchmark (pos-neg (:turnover-delta-val benchmark))]]
+                [:td [:span.benchmark (:turnover-delta benchmark)]]
+                [:td [:span.benchmark (:employee-count benchmark)]]
+                [:td [:span.benchmark (pos-neg (:employee-count-delta-val benchmark))]]
+                [:td [:span.benchmark (:employee-count-delta benchmark)]]
+]
                ]]]]))))

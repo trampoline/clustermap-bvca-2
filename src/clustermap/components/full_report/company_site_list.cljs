43dea74aadd1c3a283665e43ba3ea1a5e96a9d4b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-03-01 14:33:27 +0000

    ordering investments list

--- src/clustermap/components/full_report/company_site_list.cljs
+++ src/clustermap/components/full_report/company_site_list.cljs
@@ -1,5 +1,6 @@
 (ns clustermap.components.full-report.company-site-list
-  (:require [om.core :as om :include-macros true]
+  (:require [cljs.core.async :refer [put!]]
+            [om.core :as om :include-macros true]
             [sablono.core :as html :refer [html] :include-macros true]
             [clustermap.formats.number :as nf :refer [fnum]]
             [clustermap.formats.money :as mf :refer [fmoney]]
@@ -19,18 +20,39 @@
        [:td (link-fn :constituency {:boundaryline_id (:boundaryline_id investment)
                                     :compact_name (:boundaryline_compact_name investment)})]]))))
 
+(defn order-col
+  [comm collection view-update-key title col]
+  (let [current-order (:order collection)
+        current-order (if (sequential? current-order) (first current-order) current-order)
+        current-order-key (-> current-order keys first)
+        current-order-dir (-> current-order vals first)]
+    (html
+     [:a
+      {:onClick (fn [e]
+                  (.preventDefault e)
+                  (condp = current-order-dir
+                    "asc" (put! comm [view-update-key {:order {col :desc}}])
+                    "desc" (put! comm [view-update-key {:order {col :asc}}])
+                    (put! comm [view-update-key {:order {col :desc}}])))}
+      title
+      (if (= current-order-key col)
+        (condp = current-order-dir
+          "asc" [:i.icon-asc]
+          [:i.icon-desc]))])))
+
 (defn company-site-list
   [investments owner opts]
-  (om/component
-       (html
-        [:div.full-report-list
-         [:div.table-responsive
-          [:table.table
-           [:thead
-            [:tr
-             [:th "Portfolio Company"]
-             [:th "Postcode"]
-             [:th "Investor"]
-             [:th "Constituency"]]]
-           [:tbody
-            (om/build-all investment (:records investments) {:key :investment_uid :opts opts})]]]])))
+  (let [comm (om/get-shared owner :comm)]
+    (om/component
+     (html
+      [:div.full-report-list
+       [:div.table-responsive
+        [:table.table
+         [:thead
+          [:tr
+           [:th (order-col comm investments :update-selection-investments-table-view "Portfolio Company" :!portfolio_company_name_na)]
+           [:th (order-col comm investments :update-selection-investments-table-view "Postcode" :?portfolio_company_site_postcode)]
+           [:th (order-col comm investments :update-selection-investments-table-view "Investor" :?investor_company_name_na)]
+           [:th (order-col comm investments :update-selection-investments-table-view "Constituency" :?boundaryline_compact_name_na)]]]
+         [:tbody
+          (om/build-all investment (:records investments) {:key :investment_uid :opts opts})]]]]))))

ca6ae1fd0c49e82951b3b9bc19d58729b92391b4
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-03-14 19:04:52 +0000

    oops... send strings, not cljs symbols

--- src/clustermap/ganalytics.cljs
+++ src/clustermap/ganalytics.cljs
@@ -4,6 +4,6 @@
   [path]
   (let [ua-code (some-> js/window .-config .-ua .-code)]
     (when (and js/window.ga ua-code)
-      (js/ga 'send' 'pageview' path)
+      (js/ga "send" "pageview" path)
 ;;      (.log js/console (clj->js ["GA pageview sent: " path]))
       )))

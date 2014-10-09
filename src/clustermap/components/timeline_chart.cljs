f0e4caf9c3fcc25c0b16e13398bb7a8ce73b6c83
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-10-09 17:11:36 +0100

    timeline-chart component

--- src/clustermap/components/timeline_chart.cljs
+++ src/clustermap/components/timeline_chart.cljs
@@ -1,22 +1,25 @@
 (ns clustermap.components.timeline-chart
   (:require-macros [hiccups.core :as hiccups])
   (:require
-   [clojure.set :as set]
    [om.core :as om :include-macros true]
-   [om.dom :as dom :include-macros true]
+   [om-tools.core :refer-macros [defcomponent]]
    [jayq.core :refer [$]]
    [sablono.core :as html :refer-macros [html]]
-   [hiccups.runtime :as hiccupsrt]
+   [clustermap.api :as api]
+   [clustermap.ordered-resource :as ordered-resource]
    [clustermap.formats.number :as num]
    [clustermap.formats.money :as money]))
 
 (defn create-chart
   [data node {:keys [y0-title y1-title] :as opts}]
-  (let [x-labels (map :date data)
+  (.log js/console (clj->js ["TIMELINE: " data]))
+  (let [x-labels (->> data (map :timeline) (map #(js/Date. %)) (map #(.getYear %)) (map #(+ 1900 %)))
         stats (map :stats data)
         y-median (map (comp #(num/round-decimal % 0) #(get-in % [:stats :median])) data)
         y-mean (map (comp #(num/round-decimal % 0) #(get-in % [:stats :mean])) data)
-        y-total (map (comp #(num/round-decimal % 0) #(get-in % [:stats :total])) data)
+        ;; y-total (map (comp #(num/round-decimal % 0) #(get-in % [:stats :total])) data)
+        y-total (map :!turnover data)
+
         ;; y-total (into [] (concat (butlast yt) [(merge (last yt) {:color "#FF9900" :name "Not all data received for year"})]))
         ]
 
@@ -51,30 +54,92 @@
                     ;;  :data y-count}
                     ]})))))
 
-(defn timeline-chart
-  [data owner {:keys [id] :as opts}]
-  (reify
-    om/IRender
-    (render [this]
-      (html [:div.timeline-chart {:id id :ref "chart"}]))
+(defn- request-timeline-data
+  [resource index index-type filter-spec bounds time-variable interval metric-variables]
+  (ordered-resource/api-call resource
+                             api/timeline
+                             index
+                             index-type
+                             filter-spec
+                             bounds
+                             time-variable
+                             nil
+                             nil
+                             interval
+                             metric-variables)
+  )
 
-    om/IDidMount
-    (did-mount [this]
-      (let [node (om/get-node owner)]
-        (create-chart data (om/get-node owner "chart") opts)
+(defcomponent timeline-chart
+  [{{{index :index
+      index-type :index-type
+      time-variable :time-variable
+      measure-variables :measure-variables
+      interval :interval
+      :as controls} :controls
+      timeline-data :timeline-data
+     :as timeline-chart} :timeline-chart
+    {filter-by-view :filter-by-view
+     compiled-filter :compiled
+     :as filter-spec} :filter-spec
+     bounds :bounds
+    :as props}
+   owner {:keys [id] :as opts}]
 
-        (-> js/document
-            $
-            (.on "clustermap-change-view" (fn [e]
-                                            ;; only reflow charts when they are visible
-                                            ;; they disappear otherwise
-                                            (let [chart (-> (om/get-node owner "chart") $)]
-                                              (when (.is chart ":visible")
-                                                (-> chart
-                                                    .highcharts
-                                                    .reflow))))))))
+  (render
+   [_]
+   (html [:div.timeline-chart {:id id :ref "chart"}]))
 
-    om/IDidUpdate
-    (did-update [this prev-props prev-state]
-      (let [root-node (om/get-node owner)]
-        (create-chart data (om/get-node owner "chart") opts)))))
+  (did-mount
+   [_]
+   (let [tdr (ordered-resource/make-discard-stale-resource "timeline-data-resource")]
+     (om/set-state! owner :timeline-data-resource tdr)
+     (ordered-resource/retrieve-responses tdr (fn [{data :data :as response}] (om/update! timeline-chart [:timeline-data] data))))
+
+   (let [node (om/get-node owner)]
+     (-> js/document
+         $
+         (.on "clustermap-change-view" (fn [e]
+                                         ;; only reflow charts when they are visible
+                                         ;; they disappear otherwise
+                                         (let [chart (-> (om/get-node owner "chart") $)]
+                                           (when (.is chart ":visible")
+                                             (-> chart
+                                                 .highcharts
+                                                 .reflow))))))))
+  (will-update
+   [_
+    {{{next-index :index
+       next-index-type :index-type
+       next-time-variable :time-variable
+       next-measure-variables :measure-variables
+       next-interval :interval
+       :as next-controls} :controls
+       next-timeline-data :timeline-data
+       :as next-timeline-chart} :timeline-chart
+       {next-filter-by-view :filter-by-view
+        next-compiled-filter :compiled
+        :as next-filter-spec} :filter-spec
+        next-bounds :bounds
+        :as props}
+    {next-timeline-data-resource :timeline-data-resource}]
+
+   (.log js/console (clj->js ["FILTER_SPEC: " next-filter-spec]))
+   (when (or (not next-timeline-data)
+             (not= next-controls controls)
+             (not= next-filter-spec filter-spec)
+             (and next-filter-by-view (not= next-bounds bounds)))
+
+     (request-timeline-data next-timeline-data-resource
+                            next-index
+                            next-index-type
+                            next-compiled-filter
+                            (when next-filter-by-view next-bounds)
+                            next-time-variable
+                            next-interval
+                            next-measure-variables)))
+
+  (did-update
+   [_ _ _]
+   (let [root-node (om/get-node owner)]
+     (create-chart timeline-data (om/get-node owner "chart") opts)))
+  )

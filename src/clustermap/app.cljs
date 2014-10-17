0f03258ed2bd02e326ce9d8667c2a78502cdd1cc
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-10-17 01:38:28 +0100

    use NUTS boundaries for coarser zooms

--- src/clustermap/app.cljs
+++ src/clustermap/app.cljs
@@ -54,7 +54,8 @@
 
          :map {:type :geoport
                :datasource "companies"
-               :boundaryline-collections [[0 "uk_regions"] [5 "uk_counties"] [7 "uk_boroughs"] [10 "uk_wards"]]
+               :boundaryline-collections [[0 "nuts_0"] [4 "nuts_1"] [6 "nuts_2"] [7 "nuts_3"] [8 "uk_boroughs"] [10 "uk_wards"]]
+               ;; :boundaryline-collections [[0 "uk_regions"] [5 "uk_counties"] [7 "uk_boroughs"] [10 "uk_wards"]]
                :controls {:initial-bounds [[59.54 2.3] [49.29 -11.29]]
                           :zoom nil
                           :bounds nil

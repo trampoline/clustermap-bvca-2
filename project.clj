1dc71c554de741589a24fc5e7cab775823675922
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-10-21 16:09:05 +0100

    update deps

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2322" :scope "provided"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha" :scope "provided"]
-                 [weasel "0.4.0-SNAPSHOT"]
-                 [figwheel "0.1.4-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-2356" :scope "provided"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
+                 [weasel "0.4.1"]
+                 [figwheel "0.1.5-SNAPSHOT"]
                  [domina "1.0.2"]
                  [jayq "2.5.2"]
-                 [om "0.7.3"]
-                 [prismatic/om-tools "0.3.2" :exclusions [org.clojure/clojure]]
+                 [om "0.8.0-alpha1"]
+                 [prismatic/om-tools "0.3.6" :exclusions [org.clojure/clojure]]
                  [sablono "0.2.22"]
                  [hiccups "0.3.0"]
                  [secretary "1.2.0"]
-                 [com.andrewmcveigh/cljs-time "0.2.1"]
+                 [com.andrewmcveigh/cljs-time "0.2.2"]
                  ]
 
 ;;  :exclusions [org.clojure/google-closure-library
@@ -30,7 +30,7 @@
   :source-paths ["src"]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-figwheel "0.1.4-SNAPSHOT"]]
+            [lein-figwheel "0.1.5-SNAPSHOT"]]
 
   :cljsbuild {
               :builds {:none

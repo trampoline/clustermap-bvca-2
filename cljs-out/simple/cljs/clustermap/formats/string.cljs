b0597d431c191e38c2e2957fad2431e2f437740a
Christopher McDevitt <christophermcdevitt@gmail.com>
2015-10-28 17:13:47 +0000

    remove simple/cljs

--- cljs-out/simple/cljs/clustermap/formats/string.cljs
+++
@@ -1,6 +0,0 @@
-(ns clustermap.formats.string)
-
-(defn pluralize [n word & [irreg]]
-  (if (== n 1)
-    word
-    (or irreg (str word "s"))))
--- cljs-out/simple/cljs/clustermap/formats/time.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2356
-goog.provide('clustermap.formats.time');
-goog.require('cljs.core');
-clustermap.formats.time.get_year = (function get_year(t){var G__13871 = t;var G__13871__$1 = (((G__13871 == null))?null:(new Date(G__13871)));var G__13871__$2 = (((G__13871__$1 == null))?null:G__13871__$1.getYear());var G__13871__$3 = (((G__13871__$2 == null))?null:(G__13871__$2 + (1900)));return G__13871__$3;
-});
